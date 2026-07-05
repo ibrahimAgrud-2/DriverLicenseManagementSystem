fec35f1afe21115387b333f3b5ec6569cfacd418
ibrahim <ibrahimagrud7@gmail.com>
2026-07-05 17:43:31 +0300

    Email validiting function added to utility class

--- DVLD/Utility.cs
+++ DVLD/Utility.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 
 namespace DVLD
 {
-    internal class Utility
+    public class Utility
     {
+        public bool IsEmailValid(string email)
+        {
+            var regex = new Regex(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$");
+            return regex.IsMatch(email);
+        }
+
+
     }
 }
