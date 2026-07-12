ee06c3c78672999954647ff4d8ca690a3967347a
ibrahim <ibrahimagrud7@gmail.com>
2026-07-12 08:38:30 +0300

    WIP: DL, Query join people user

--- DVLD_DataAccessLayer/UserDataAccess.cs
+++ DVLD_DataAccessLayer/UserDataAccess.cs
@@ -12,7 +12,7 @@ namespace DVLD_DataAccessLayer
             DataTable dt = new DataTable();
 
             SqlConnection connection = new SqlConnection(DataAccessSettings.connectionString);
-            string sqlQuery = "select * from users";
+            string sqlQuery = "select * from users join people on user.PersonID=People.PersonID";
 
             SqlCommand cmd = new SqlCommand(sqlQuery, connection);
 
