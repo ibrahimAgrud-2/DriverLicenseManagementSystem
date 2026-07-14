eedeacbe9953124c9cf515f941d24dd88b1d2339
ibrahim <ibrahimagrud7@gmail.com>
2026-07-14 11:06:30 +0300

    ctrl Login Info added

---
+++ DVLD/Users/Controls/ctrlLoginInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using DVLD_BusinessLayer;
+using System.Windows.Forms;
+
+namespace DVLD.Users.Controls
+{
+    public partial class ctrlLoginInfo : UserControl
+    {
+        public ctrlLoginInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void ctrlLoginInfo_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
