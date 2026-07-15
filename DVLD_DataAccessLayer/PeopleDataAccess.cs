a72a025dff16cb21a54039bc6f801a731c8cc308
ibrahim <ibrahimagrud7@gmail.com>
2026-07-15 19:47:00 +0300

    DL People: comment added

--- DVLD_DataAccessLayer/PeopleDataAccess.cs
+++ DVLD_DataAccessLayer/PeopleDataAccess.cs
@@ -14,6 +14,8 @@ namespace DVLD_DataAccessLayer
             DataTable dt = new DataTable();
 
             SqlConnection connection = new SqlConnection(DataAccessSettings.connectionString);
+            
+            //Query'deki field sırası önemli. Çünkü bu sırayla dgv'de gözükecek.
             string sqlQuery = @"select PersonID,NationalNo,FirstName,SecondName,ThirdName,LastName,DateOfBirth,
                     Gender=(Case People.Gender when 0 then 'male'  when 1 then 'female' else 'unknown' end ),
                      CountryName,Address,CountryID, Phone,Email,ImagePath from People inner join Countries on Countries.CountryID=People.NationalityCountryID";
@@ -208,6 +210,8 @@ namespace DVLD_DataAccessLayer
 
             return false;
         }
+
+
 
         public static bool IsPersonExist(int personID)
         {
