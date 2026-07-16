460351bd1d3d6c45bd53f7553bfa794036582796
ibrahim <ibrahimagrud7@gmail.com>
2026-07-16 22:51:33 +0300

    ctrlPersonCardWithFilter fire event when load succesed

--- DVLD/People/Controls/ctrlPersonCardWithFilter.cs
+++ DVLD/People/Controls/ctrlPersonCardWithFilter.cs
@@ -12,6 +12,18 @@ namespace DVLD.People.Controls
             InitializeComponent();
         }
 
+
+        public event Action<int> OnPersonLoaded;
+        // Create a protected method to raise the event with a parameter
+        protected virtual void PersonLoaded(int PersonID)
+        {
+            Action<int> handler = OnPersonLoaded;
+            if (handler != null)
+            {
+                handler(PersonID); // Raise the event with the parameter
+            }
+        }
+
         private void ctrlFindUser_Load(object sender, EventArgs e)
         {
             cbFilterBy.SelectedIndex = 0;
@@ -33,6 +45,23 @@ namespace DVLD.People.Controls
                 gbFilter.Enabled = _FilterEnabled;
             }
         }
+
+
+        //Bazı durumlarda add butonunu göstermemek isteyebiliriz.
+        private bool _ShowAddPersonButton = true;
+        public bool ShowAddPersonButton
+        {
+            get
+            {
+                return _ShowAddPersonButton;
+            }
+            set
+            {
+                _ShowAddPersonButton = value;
+                btnAddNewPerson.Visible = _ShowAddPersonButton;
+            }
+        }
+
 
         public int personID
         {
@@ -66,7 +95,13 @@ namespace DVLD.People.Controls
                     this.ctrlPersonInformation1.LoadPersonInfo(Convert.ToInt32(txtFilter.Text));
                     break;
             }
-   
+
+            //OnPersonLoaded ile Bu evente abone olan biri var mı kontrol ediypruz. YOksa çöker. 2.kontrol ise eğer filtreleme kontrol edilmişmi onu kontrol ediyoruz. 3.Konrol de ise yükleme başarılımı onu kontrol ediyoruz
+
+            if (OnPersonLoaded!=null&&FilterEnabled&&this.ctrlPersonInformation1.PersonID>0)
+            {
+                OnPersonLoaded(this.ctrlPersonInformation1.PersonID);
+            }
         }
 
     
