8578ff14e7647e13dd57cbabebb1e21cd276663e
ibrahim <ibrahimagrud7@gmail.com>
2026-07-16 23:16:12 +0300

    frm Find Person completed

--- DVLD/People/frmFindPerson.Designer.cs
+++ DVLD/People/frmFindPerson.Designer.cs
@@ -29,24 +29,48 @@
         private void InitializeComponent()
         {
             this.ctrlPersonCardWithFilter1 = new DVLD.People.Controls.ctrlPersonCardWithFilter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ctrlPersonCardWithFilter1
             // 
-            this.ctrlPersonCardWithFilter1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ctrlPersonCardWithFilter1.FilterEnabled = true;
-            this.ctrlPersonCardWithFilter1.Location = new System.Drawing.Point(0, 0);
-            this.ctrlPersonCardWithFilter1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ctrlPersonCardWithFilter1.Location = new System.Drawing.Point(-3, 72);
+            this.ctrlPersonCardWithFilter1.Margin = new System.Windows.Forms.Padding(4);
             this.ctrlPersonCardWithFilter1.Name = "ctrlPersonCardWithFilter1";
             this.ctrlPersonCardWithFilter1.ShowAddPersonButton = true;
             this.ctrlPersonCardWithFilter1.Size = new System.Drawing.Size(1200, 536);
             this.ctrlPersonCardWithFilter1.TabIndex = 0;
+            this.ctrlPersonCardWithFilter1.OnPersonLoaded += new System.Action<int>(this.ctrlPersonCardWithFilter1_OnPersonLoaded);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(518, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Find Person";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1006, 614);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(183, 56);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // frmFindPerson
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 536);
+            this.ClientSize = new System.Drawing.Size(1201, 682);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ctrlPersonCardWithFilter1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -55,11 +79,14 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmFindPerson";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Controls.ctrlPersonCardWithFilter ctrlPersonCardWithFilter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
     }
 }
--- DVLD/People/frmFindPerson.cs
+++ DVLD/People/frmFindPerson.cs
@@ -12,9 +12,26 @@ namespace DVLD.People
 {
     public partial class frmFindPerson : Form
     {
+
+        public delegate void DataBackEventHandler(object sender,int PersonID);
+
+        public event DataBackEventHandler DataBack;
+
+
         public frmFindPerson()
         {
             InitializeComponent();
+        }
+
+        private void ctrlPersonCardWithFilter1_OnPersonLoaded(int obj)
+        {
+ 
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DataBack?.Invoke(this, ctrlPersonCardWithFilter1.personID);
+
         }
     }
 }
