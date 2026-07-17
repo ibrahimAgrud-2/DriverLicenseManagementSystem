ca3d4b77d878c0459d2c075c7b8388b7ab78544d
ibrahim <ibrahimagrud7@gmail.com>
2026-07-17 05:49:20 +0300

    frm show details done

--- DVLD/People/frmPersonDetail.cs
+++ DVLD/People/frmPersonDetail.cs
@@ -12,19 +12,26 @@ namespace DVLD
 {
     public partial class frmPersonDetail : Form
     {
+
+
+
         private int _PersonID;
+
+        private string _NationalNo;
+
         public frmPersonDetail(int personID)
         {
             InitializeComponent();
             _PersonID = personID;
-        }
-
-        private void frmPersonDetail_Load(object sender, EventArgs e)
-        {
-            
             this.ctrlPersonInformation1.LoadPersonInfo(_PersonID);
-
         }
+        public frmPersonDetail(string  nationalNo)
+        {
+            InitializeComponent();
+            _NationalNo = nationalNo;
+            this.ctrlPersonInformation1.LoadPersonInfo(_NationalNo);
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
