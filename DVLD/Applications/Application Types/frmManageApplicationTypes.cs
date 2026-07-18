4e399915adb73c7cc29fe68b9508dec38c558457
ibrahim <ibrahimagrud7@gmail.com>
2026-07-18 17:20:16 +0300

    form error

---
+++ DVLD/Applications/Application Types/frmManageApplicationTypes.cs
@@ -0,0 +1,62 @@
+﻿using DVLD.Applications.Application_Types;
+using DVLD_BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DVLD.Application.Application_Types
+{
+    public partial class frmManageApplications : Form
+    {
+        public frmManageApplications()
+        {
+            InitializeComponent();
+        }
+
+        DataTable _AppList;
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private Dictionary<string, string> _ColumnNames = new Dictionary<string, string>
+            {
+              { "applicationTypeID", "Application Type ID" },
+              { "applicationTypeTitle", "Application Type Title" },
+              { "ApplicationFees", "Application Fees" }
+            };
+        private void _SetColumnNames()
+        {
+            foreach (KeyValuePair<string, string> dict in _ColumnNames)
+            {
+                dgvAppList.Columns[dict.Key].HeaderText = dict.Value;
+            }
+        }
+
+        private void _RefreshList()
+        {
+            _AppList = ApplicationTypes.getApplicationTypeRecords();
+            dgvAppList.DataSource = _AppList;
+            lblRecords.Text = dgvAppList.RowCount.ToString();
+
+        }
+        private void frmManageApplications_Load(object sender, EventArgs e)
+        {
+            _RefreshList();
+            _SetColumnNames();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(dgvAppList.SelectedRows[0].Cells[0].Value.ToString(), out int selectedPersonID))
+            {
+                frmEditApplicationType frm = new frmEditApplicationType();
+                frm.ShowDialog();
+                _RefreshList();
+            }
+
+        }
+    }
+}
