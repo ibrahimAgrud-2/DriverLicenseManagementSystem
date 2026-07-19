90b2e21712d37a4ca73b21e61b6bb2ad3ba4414d
ibrahim <ibrahimagrud7@gmail.com>
2026-07-19 20:38:53 +0300

    BL Application:  use only composition

--- DVLD_BusinessLayer/Applications.cs
+++ DVLD_BusinessLayer/Applications.cs
@@ -18,8 +18,21 @@ namespace DVLD_BusinessLayer
         public DateTime ApplicationDate { set; get; }
 
         public int ApplicationTypeID { set; get; }
-        public ApplicationTypes ApplicationType { set; get; }
+        public ApplicationTypes ApplicationTypeInfo { set; get; }
 
+        public enum enApplicationStatus {New=1,Canceled=2,Completed=3 };
+        public enApplicationStatus ApplicationStatus;
+
+        public  DateTime LastStatusDate{ set; get; }
+
+        public double PaidFees { set; get; }
+
+
+        public int CreatedByUserID { set; get; }
+        public ApplicationTypes UserInfo { set; get; }
+
+        public enum enMode { enAddNew = 1, enUpdate = 2};
+        public enMode Mode;
 
 
 
@@ -34,31 +47,31 @@ namespace DVLD_BusinessLayer
 
         public Applications()
         {
-            this.applicationID = -1;
-            this.applicantPersonID = -1;
-            this.createdByUserID = -1;
-            this.applicationDate=DateTime.Now;
-            this.applicationTypeID = ApplicationTypes.enApplicationType.NewLocalDrivingLicenseService;
-            this.applicationStatus = enApplicationStatus.New;
-            this.lastStatusDate = DateTime.Now;
-            this.paidFee = 0.0;
-            this.mode=enMode.enAddNew;
+            this.ApplicationID = -1;
+            this.ApplicantPersonID = -1;
+            this.CreatedByUserID = -1;
+            this.ApplicationDate=DateTime.Now;
+            this.ApplicationTypeID = -1;
+            this.ApplicationStatus = enApplicationStatus.New;
+            this.LastStatusDate = DateTime.Now;
+            this.PaidFees = 0.0;
+            this.Mode = enMode.enAddNew;
         }
 
-        private Applications(int applicationID, int applicantPersonID, DateTime ApplicationDate, ApplicationTypes.enApplicationType applicationTypeID,
+        private Applications(int applicationID, int applicantPersonID, DateTime ApplicationDate, int applicationTypeID,
            enApplicationStatus applicationStatus, DateTime LastStatusDate, double paidFee, int createdByUserID)
         {
 
-            this.applicationID = applicationID;
-            this.applicantPersonID = applicantPersonID;
-            this.applicationDate = ApplicationDate;
-            this.applicationTypeID = applicationTypeID;
-            this.applicationStatus = applicationStatus;
-            this.lastStatusDate = LastStatusDate;
-            this.paidFee = paidFee;
+            this.ApplicationID = applicationID;
+            this.ApplicantPersonID = applicantPersonID;
+            this.ApplicationDate = ApplicationDate;
+            this.ApplicationTypeID = applicationTypeID;
+            this.ApplicationStatus = applicationStatus;
+            this.LastStatusDate = LastStatusDate;
+            this.PaidFees = paidFee;
             ApplicationTypeInfo = ApplicationTypes.Find(applicationTypeID);
-            this.createdByUserID = createdByUserID;
-            this.mode = enMode.enUpdate;
+            this.CreatedByUserID = createdByUserID;
+            this.Mode = enMode.enUpdate;
 
         }
         public static DataTable getApplicationsRecord()
@@ -84,7 +97,7 @@ namespace DVLD_BusinessLayer
             if (ApplicationsDataAccess.findApplication( applicationID, ref  applicantPersonID, ref  applicationDate, ref  applicationTypeID, ref
             applicationStatus, ref lastStatusDate, ref  paidFee, ref  createdByUserID))
             {
-                return new Applications(applicationID,  applicantPersonID,  applicationDate, (ApplicationTypes.enApplicationType)applicationTypeID, 
+                return new Applications(applicationID,  applicantPersonID,  applicationDate, applicationTypeID, 
             (enApplicationStatus)applicationStatus,  lastStatusDate,  paidFee,  createdByUserID);
 
             }
@@ -96,27 +109,26 @@ namespace DVLD_BusinessLayer
         {
 
             //Başvuru ücretini elle girmemek için bu kodu ekledirk.
-            ApplicationTypes type1 = ApplicationTypes.Find(this.applicationTypeID);
+            ApplicationTypes type1 = ApplicationTypes.Find(this.ApplicationTypeID);
             if (type1 == null)
             {
                 return false;
             }
-            this.paidFee = type1.applicationFee;
+            this.PaidFees = type1.applicationFee;
 
             //Normalde bu bilgi o anki giriş yapan kullanıcı bilgilerinde çekilir ama şu anda giriş ekranı daha yok. 
             //Giriş ekranı olduğunda kullanıcı aktif kullanıcı bilgilerinden çekilir.
-            this.createdByUserID = 1;
-            this.applicationID = ApplicationsDataAccess.addApplication(this.applicantPersonID, (int)this.applicationTypeID, 
-                Convert.ToByte(this.applicationStatus), this.lastStatusDate, this.paidFee, this.createdByUserID);
-            return (this.applicationID != -1);
+            this.CreatedByUserID = 1;
+            this.ApplicationID = ApplicationsDataAccess.addApplication(ApplicantPersonID,ApplicationTypeID,Convert.ToByte(ApplicationStatus),LastStatusDate,PaidFees,CreatedByUserID);
+            return (this.ApplicationID != -1);
 
         }
 
         //Update yaparken lastStatus güncellenmeli.
         private bool _updateApplication()
         {
-            this.lastStatusDate = DateTime.Now;
-            return ApplicationsDataAccess.updateApplicationInfo(this.applicationID,this.applicantPersonID, this.applicationDate, (int)this.applicationTypeID, Convert.ToByte(this.applicationStatus), this.lastStatusDate, this.paidFee, this.createdByUserID);
+            this.LastStatusDate = DateTime.Now;
+            return ApplicationsDataAccess.updateApplicationInfo(this.ApplicationID,this.ApplicantPersonID, this.ApplicationDate, this.ApplicationTypeID, Convert.ToByte(this.ApplicationStatus), this.LastStatusDate, this.PaidFees, this.CreatedByUserID);
         }
 
 
@@ -125,7 +137,7 @@ namespace DVLD_BusinessLayer
         {
             return ApplicationsDataAccess.isApplicationExistByID(applicationID);
         }
-        public static bool isApplicationExistByPersonID(int personID,ApplicationTypes.enApplicationType appType)
+        public static bool isApplicationExistByPersonID(int personID,int appType)
         {
            return ApplicationsDataAccess.isApplicationExistByPersonID(personID, (int)appType);
         }
@@ -141,12 +153,12 @@ namespace DVLD_BusinessLayer
 
         public bool save()
         {
-            switch (this.mode)
+            switch (this.Mode)
             {
                 case enMode.enAddNew:
                     if (_addNewApplication())
                     {
-                        this.mode = enMode.enUpdate;
+                        this.Mode = enMode.enUpdate;
                         return true;
                     }
                     else
