f51e1e0c4bd38222313bae95c1d463441b62680a
ibrahim <ibrahimagrud7@gmail.com>
2026-07-20 13:07:43 +0300

    ctrl LDLA info added

---
+++ DVLD/Applications/New Local Driving License Application/Controls/ctrlLDLAInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Applications.New_Local_Driving_License_Application.Controls
+{
+    public partial class ctrlLDLAInfo : UserControl
+    {
+        public ctrlLDLAInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
