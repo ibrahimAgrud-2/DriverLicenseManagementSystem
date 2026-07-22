c4420c46cc3898fa7f3c0edae331421a19d787c6
ibrahim <ibrahimagrud7@gmail.com>
2026-07-22 13:03:06 +0300

    ctrl Application info completed

---
+++ DVLD/Applications/Controls/frmApplicationInfo.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace DVLD.Applications.Controls
+{
+    partial class frmApplicationInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmApplicationInfo));
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblFees = new System.Windows.Forms.Label();
+            this.lblAppStatus = new System.Windows.Forms.Label();
+            this.lnklblEditPersonInfo = new System.Windows.Forms.LinkLabel();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCreatedByUserID = new System.Windows.Forms.Label();
+            this.lblAppDate = new System.Windows.Forms.Label();
+            this.lbID = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAppType = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblApplicantPersonFullName = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblAppLastStatusDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblApplicantPersonFullName);
+            this.groupBox1.Controls.Add(this.lblAppType);
+            this.groupBox1.Controls.Add(this.lblFees);
+            this.groupBox1.Controls.Add(this.lblAppStatus);
+            this.groupBox1.Controls.Add(this.lnklblEditPersonInfo);
+            this.groupBox1.Controls.Add(this.lblCountry);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblAppLastStatusDate);
+            this.groupBox1.Controls.Add(this.lblCreatedByUserID);
+            this.groupBox1.Controls.Add(this.lblAppDate);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.lbID);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(927, 312);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lblFees
+            // 
+            this.lblFees.AutoSize = true;
+            this.lblFees.Location = new System.Drawing.Point(215, 110);
+            this.lblFees.Name = "lblFees";
+            this.lblFees.Size = new System.Drawing.Size(60, 24);
+            this.lblFees.TabIndex = 80;
+            this.lblFees.Text = "?????";
+            // 
+            // lblAppStatus
+            // 
+            this.lblAppStatus.AutoSize = true;
+            this.lblAppStatus.Location = new System.Drawing.Point(215, 69);
+            this.lblAppStatus.Name = "lblAppStatus";
+            this.lblAppStatus.Size = new System.Drawing.Size(60, 24);
+            this.lblAppStatus.TabIndex = 81;
+            this.lblAppStatus.Text = "?????";
+            // 
+            // lnklblEditPersonInfo
+            // 
+            this.lnklblEditPersonInfo.AutoSize = true;
+            this.lnklblEditPersonInfo.Enabled = false;
+            this.lnklblEditPersonInfo.Location = new System.Drawing.Point(517, 217);
+            this.lnklblEditPersonInfo.Name = "lnklblEditPersonInfo";
+            this.lnklblEditPersonInfo.Size = new System.Drawing.Size(152, 24);
+            this.lnklblEditPersonInfo.TabIndex = 76;
+            this.lnklblEditPersonInfo.TabStop = true;
+            this.lnklblEditPersonInfo.Text = "View People Info";
+            this.lnklblEditPersonInfo.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklblEditPersonInfo_LinkClicked);
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Location = new System.Drawing.Point(609, 154);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(60, 24);
+            this.lblCountry.TabIndex = 85;
+            this.lblCountry.Text = "?????";
+            // 
+            // label1
+            // 
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(25, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 32);
+            this.label1.TabIndex = 74;
+            this.label1.Text = "Fee";
+            // 
+            // label2
+            // 
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(25, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 32);
+            this.label2.TabIndex = 75;
+            this.label2.Text = "Status";
+            // 
+            // lblCreatedByUserID
+            // 
+            this.lblCreatedByUserID.Image = ((System.Drawing.Image)(resources.GetObject("lblCreatedByUserID.Image")));
+            this.lblCreatedByUserID.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblCreatedByUserID.Location = new System.Drawing.Point(423, 154);
+            this.lblCreatedByUserID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCreatedByUserID.Name = "lblCreatedByUserID";
+            this.lblCreatedByUserID.Size = new System.Drawing.Size(142, 26);
+            this.lblCreatedByUserID.TabIndex = 73;
+            this.lblCreatedByUserID.Text = "Created By: ";
+            // 
+            // lblAppDate
+            // 
+            this.lblAppDate.AutoSize = true;
+            this.lblAppDate.Location = new System.Drawing.Point(609, 34);
+            this.lblAppDate.Name = "lblAppDate";
+            this.lblAppDate.Size = new System.Drawing.Size(60, 24);
+            this.lblAppDate.TabIndex = 83;
+            this.lblAppDate.Text = "?????";
+            // 
+            // lbID
+            // 
+            this.lbID.AutoSize = true;
+            this.lbID.Location = new System.Drawing.Point(210, 31);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(60, 24);
+            this.lbID.TabIndex = 82;
+            this.lbID.Text = "?????";
+            // 
+            // label13
+            // 
+            this.label13.Image = ((System.Drawing.Image)(resources.GetObject("label13.Image")));
+            this.label13.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label13.Location = new System.Drawing.Point(415, 34);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(150, 26);
+            this.label13.TabIndex = 78;
+            this.label13.Text = "App. Date";
+            // 
+            // label5
+            // 
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Location = new System.Drawing.Point(34, 31);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 32);
+            this.label5.TabIndex = 79;
+            this.label5.Text = "ID: ";
+            // 
+            // label4
+            // 
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(25, 154);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 32);
+            this.label4.TabIndex = 74;
+            this.label4.Text = "App.Type";
+            // 
+            // lblAppType
+            // 
+            this.lblAppType.AutoSize = true;
+            this.lblAppType.Location = new System.Drawing.Point(215, 154);
+            this.lblAppType.Name = "lblAppType";
+            this.lblAppType.Size = new System.Drawing.Size(60, 24);
+            this.lblAppType.TabIndex = 80;
+            this.lblAppType.Text = "?????";
+            // 
+            // label7
+            // 
+            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label7.Location = new System.Drawing.Point(25, 217);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 32);
+            this.label7.TabIndex = 74;
+            this.label7.Text = "Applicant: ";
+            // 
+            // lblApplicantPersonFullName
+            // 
+            this.lblApplicantPersonFullName.AutoSize = true;
+            this.lblApplicantPersonFullName.Location = new System.Drawing.Point(215, 217);
+            this.lblApplicantPersonFullName.Name = "lblApplicantPersonFullName";
+            this.lblApplicantPersonFullName.Size = new System.Drawing.Size(60, 24);
+            this.lblApplicantPersonFullName.TabIndex = 80;
+            this.lblApplicantPersonFullName.Text = "?????";
+            // 
+            // label11
+            // 
+            this.label11.Image = ((System.Drawing.Image)(resources.GetObject("label11.Image")));
+            this.label11.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label11.Location = new System.Drawing.Point(423, 91);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(142, 26);
+            this.label11.TabIndex = 77;
+            this.label11.Text = "Status Date";
+            // 
+            // lblAppLastStatusDate
+            // 
+            this.lblAppLastStatusDate.AutoSize = true;
+            this.lblAppLastStatusDate.Location = new System.Drawing.Point(609, 91);
+            this.lblAppLastStatusDate.Name = "lblAppLastStatusDate";
+            this.lblAppLastStatusDate.Size = new System.Drawing.Size(60, 24);
+            this.lblAppLastStatusDate.TabIndex = 84;
+            this.lblAppLastStatusDate.Text = "?????";
+            // 
+            // frmApplicationInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmApplicationInfo";
+            this.Size = new System.Drawing.Size(965, 347);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblFees;
+        private System.Windows.Forms.Label lblAppStatus;
+        private System.Windows.Forms.LinkLabel lnklblEditPersonInfo;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCreatedByUserID;
+        private System.Windows.Forms.Label lblAppDate;
+        private System.Windows.Forms.Label lbID;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblAppType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblApplicantPersonFullName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblAppLastStatusDate;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ DVLD/Applications/Controls/frmApplicationInfo.cs
@@ -0,0 +1,86 @@
+﻿using DVLD.Properties;
+using DVLD_BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using clsApplications = DVLD_BusinessLayer.Applications;
+
+namespace DVLD.Applications.Controls
+{
+    public partial class frmApplicationInfo : UserControl
+    {
+        public frmApplicationInfo()
+        {
+            InitializeComponent();
+        }
+
+        private int _ApplicationID = -1;
+        private clsApplications _App=null;
+
+        public int ApplicationID { get { return _ApplicationID; } }
+
+        public clsApplications SelectedApp { get { return _App; } }
+
+        private void fillObjectDataToField(clsApplications App)
+        {
+
+            lblAppDate.Text = App.ApplicationDate.ToString() ;
+            lblFees.Text = App.PaidFees.ToString();
+            lblAppLastStatusDate.Text = App.LastStatusDate.ToString();
+            lblCreatedByUserID.Text = App.CreatedByUserID.ToString();
+            lblApplicantPersonFullName.Text = App.ApplicantPerson.fullName;
+            lblAppType.Text = App.ApplicationTypeInfo.applicantTypeTitle;
+            lbID.Text = App.ID.ToString();
+            lblAppStatus.Text = App.ApplicationStatus.ToString();
+
+        }
+
+
+        private void _Load()
+        {
+            _ApplicationID = _App.ID;
+            fillObjectDataToField(_App);
+            lnklblEditPersonInfo.Enabled = true;
+
+        }
+        public void LoadAppInfo(int AppID)
+        {
+            _App = clsApplications.Find(AppID);
+            if (_App == null)
+            {
+                _ApplicationID = -1;
+                ResetForm();
+                MessageBox.Show("No Person with PersonID = " + AppID, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _Load();
+        }
+        public void ResetForm()
+        {
+            //Yükleme başarılı olup olmadığını ID ile biliyoruz. Bu yüzden yükleme başarısız olduğunda 
+            //formu temizlerken ID'i de -1 yapıyoruz.
+            _ApplicationID = -1;
+            lblAppDate.Text = "????";
+            lblFees.Text = "????";
+            lblAppLastStatusDate.Text = "????";
+            lblCreatedByUserID.Text = "????";
+            lblApplicantPersonFullName.Text = "????";
+            lblAppType.Text = "????";
+            lbID.Text = "????";
+            lblAppStatus.Text = "????";
+
+        }
+
+        private void lnklblEditPersonInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmAddUpdatePerson frm = new frmAddUpdatePerson(_App.ApplicantPersonID);
+            frm.ShowDialog();
+            LoadAppInfo(ApplicationID);
+        }
+    }
+}
