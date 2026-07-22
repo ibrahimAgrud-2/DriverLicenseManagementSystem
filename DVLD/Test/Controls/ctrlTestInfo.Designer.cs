1b8185dec0b8c47c1bde7053835295a3c8935b6f
ibrahim <ibrahimagrud7@gmail.com>
2026-07-22 16:29:05 +0300

    ctrl test appointment info added

---
+++ DVLD/Test/Controls/ctrlTestInfo.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace DVLD.Test
+{
+    partial class ctrlTestInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFees = new System.Windows.Forms.Label();
+            this.lblTrail = new System.Windows.Forms.Label();
+            this.lblApplicantName = new System.Windows.Forms.Label();
+            this.lblAppForClass = new System.Windows.Forms.Label();
+            this.lblLDLAID = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpApplicationDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lblFees
+            // 
+            this.lblFees.AutoSize = true;
+            this.lblFees.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblFees.Location = new System.Drawing.Point(198, 260);
+            this.lblFees.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFees.Name = "lblFees";
+            this.lblFees.Size = new System.Drawing.Size(67, 25);
+            this.lblFees.TabIndex = 88;
+            this.lblFees.Text = "?????";
+            // 
+            // lblTrail
+            // 
+            this.lblTrail.AutoSize = true;
+            this.lblTrail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTrail.Location = new System.Drawing.Point(198, 164);
+            this.lblTrail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTrail.Name = "lblTrail";
+            this.lblTrail.Size = new System.Drawing.Size(67, 25);
+            this.lblTrail.TabIndex = 89;
+            this.lblTrail.Text = "?????";
+            // 
+            // lblApplicantName
+            // 
+            this.lblApplicantName.AutoSize = true;
+            this.lblApplicantName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblApplicantName.Location = new System.Drawing.Point(198, 119);
+            this.lblApplicantName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblApplicantName.Name = "lblApplicantName";
+            this.lblApplicantName.Size = new System.Drawing.Size(67, 25);
+            this.lblApplicantName.TabIndex = 90;
+            this.lblApplicantName.Text = "?????";
+            // 
+            // lblAppForClass
+            // 
+            this.lblAppForClass.AutoSize = true;
+            this.lblAppForClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblAppForClass.Location = new System.Drawing.Point(198, 74);
+            this.lblAppForClass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAppForClass.Name = "lblAppForClass";
+            this.lblAppForClass.Size = new System.Drawing.Size(67, 25);
+            this.lblAppForClass.TabIndex = 91;
+            this.lblAppForClass.Text = "?????";
+            // 
+            // lblLDLAID
+            // 
+            this.lblLDLAID.AutoSize = true;
+            this.lblLDLAID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblLDLAID.Location = new System.Drawing.Point(198, 29);
+            this.lblLDLAID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLDLAID.Name = "lblLDLAID";
+            this.lblLDLAID.Size = new System.Drawing.Size(67, 25);
+            this.lblLDLAID.TabIndex = 92;
+            this.lblLDLAID.Text = "?????";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Location = new System.Drawing.Point(63, 32);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 25);
+            this.label5.TabIndex = 87;
+            this.label5.Text = "LDLA ID: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(-1, 126);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 87;
+            this.label1.Text = "Applicant Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 25);
+            this.label2.TabIndex = 87;
+            this.label2.Text = "App For Class: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(50, 214);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 25);
+            this.label3.TabIndex = 87;
+            this.label3.Text = "App. Date: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(99, 170);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 25);
+            this.label4.TabIndex = 87;
+            this.label4.Text = "Trail: ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label6.Location = new System.Drawing.Point(93, 262);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 25);
+            this.label6.TabIndex = 87;
+            this.label6.Text = "Fees: ";
+            this.label6.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dtpApplicationDate
+            // 
+            this.dtpApplicationDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dtpApplicationDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpApplicationDate.Location = new System.Drawing.Point(203, 209);
+            this.dtpApplicationDate.Name = "dtpApplicationDate";
+            this.dtpApplicationDate.Size = new System.Drawing.Size(129, 30);
+            this.dtpApplicationDate.TabIndex = 93;
+            // 
+            // ctrlTestInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dtpApplicationDate);
+            this.Controls.Add(this.lblFees);
+            this.Controls.Add(this.lblTrail);
+            this.Controls.Add(this.lblApplicantName);
+            this.Controls.Add(this.lblAppForClass);
+            this.Controls.Add(this.lblLDLAID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ctrlTestInfo";
+            this.Size = new System.Drawing.Size(565, 308);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFees;
+        private System.Windows.Forms.Label lblTrail;
+        private System.Windows.Forms.Label lblApplicantName;
+        private System.Windows.Forms.Label lblAppForClass;
+        private System.Windows.Forms.Label lblLDLAID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpApplicationDate;
+    }
+}
