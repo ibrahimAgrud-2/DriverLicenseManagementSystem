54b153333ebe03fdd53ebe7806821415a5e270fe
ibrahim <ibrahimagrud7@gmail.com>
2026-07-22 13:18:22 +0300

    make class name public

---
+++ DVLD/Applications/New Local Driving License Application/Controls/ctrlLocalDrivinglicenseApplicationInfo.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace DVLD.Applications.New_Local_Driving_License_Application.Controls
+{
+    partial class ctrlLocalDrivinglicenseApplicationInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ctrlLocalDrivinglicenseApplicationInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ctrlLocalDrivinglicenseApplicationInfo";
+            this.Size = new System.Drawing.Size(952, 356);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
