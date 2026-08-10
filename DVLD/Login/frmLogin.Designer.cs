c8c9602ff5aa23f8a4670dcfd2d19c271b8694a6
ibrahim <ibrahimagrud7@gmail.com>
2026-08-10 16:53:13 +0300

    WIP: login scree UI

--- DVLD/Login/frmLogin.Designer.cs
+++ DVLD/Login/frmLogin.Designer.cs
@@ -28,44 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.cbRemeberMe = new System.Windows.Forms.CheckBox();
             this.label5 = new System.Windows.Forms.Label();
             this.txtUserName = new Guna.UI2.WinForms.Guna2TextBox();
             this.maskedTextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
             this.btnLogin = new Guna.UI2.WinForms.Guna2Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(204)))), ((int)(((byte)(53)))));
-            this.pictureBox1.Location = new System.Drawing.Point(0, -3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(456, 543);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.SystemColors.Control;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Brown;
-            this.label4.Location = new System.Drawing.Point(582, 183);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(233, 25);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Login To Your Account";
             // 
             // cbRemeberMe
             // 
             this.cbRemeberMe.AutoSize = true;
             this.cbRemeberMe.Checked = true;
             this.cbRemeberMe.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbRemeberMe.Location = new System.Drawing.Point(545, 384);
+            this.cbRemeberMe.Location = new System.Drawing.Point(37, 367);
             this.cbRemeberMe.Name = "cbRemeberMe";
             this.cbRemeberMe.Size = new System.Drawing.Size(158, 29);
             this.cbRemeberMe.TabIndex = 8;
@@ -76,7 +53,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.SystemColors.Control;
-            this.label5.Location = new System.Drawing.Point(53, 171);
+            this.label5.Location = new System.Drawing.Point(165, 206);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 25);
             this.label5.TabIndex = 9;
@@ -93,12 +70,12 @@
             this.txtUserName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.txtUserName.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.txtUserName.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtUserName.Location = new System.Drawing.Point(545, 235);
+            this.txtUserName.Location = new System.Drawing.Point(37, 193);
             this.txtUserName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtUserName.Name = "txtUserName";
             this.txtUserName.PlaceholderText = "User Name";
             this.txtUserName.SelectedText = "";
-            this.txtUserName.Size = new System.Drawing.Size(308, 48);
+            this.txtUserName.Size = new System.Drawing.Size(399, 55);
             this.txtUserName.TabIndex = 10;
             // 
             // maskedTextBox1
@@ -113,66 +90,85 @@
             this.maskedTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.maskedTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.maskedTextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.maskedTextBox1.Location = new System.Drawing.Point(545, 306);
+            this.maskedTextBox1.Location = new System.Drawing.Point(37, 286);
             this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.PlaceholderText = "Password";
             this.maskedTextBox1.SelectedText = "";
-            this.maskedTextBox1.Size = new System.Drawing.Size(308, 48);
+            this.maskedTextBox1.Size = new System.Drawing.Size(399, 55);
             this.maskedTextBox1.TabIndex = 10;
             // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.BorderColor = System.Drawing.Color.Bisque;
             this.btnLogin.BorderRadius = 7;
             this.btnLogin.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnLogin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnLogin.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.btnLogin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btnLogin.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(204)))), ((int)(((byte)(53)))));
-            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btnLogin.ForeColor = System.Drawing.Color.Black;
-            this.btnLogin.Location = new System.Drawing.Point(535, 432);
+            this.btnLogin.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(20)))), ((int)(((byte)(186)))));
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10.89076F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Beige;
+            this.btnLogin.Location = new System.Drawing.Point(37, 416);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(318, 45);
+            this.btnLogin.Size = new System.Drawing.Size(399, 48);
             this.btnLogin.TabIndex = 11;
             this.btnLogin.Text = "Login";
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 18.15126F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 38);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Hello!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.89076F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Sing in to You Account";
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Snow;
-            this.ClientSize = new System.Drawing.Size(986, 536);
-            this.Controls.Add(this.btnLogin);
+            this.ClientSize = new System.Drawing.Size(462, 516);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbRemeberMe);
             this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.cbRemeberMe);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmLogin";
             this.Load += new System.EventHandler(this.frmLogin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.CheckBox cbRemeberMe;
         private System.Windows.Forms.Label label5;
         private Guna.UI2.WinForms.Guna2TextBox txtUserName;
         private Guna.UI2.WinForms.Guna2TextBox maskedTextBox1;
         private Guna.UI2.WinForms.Guna2Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
