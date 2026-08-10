ec4a306e1b4499e5db11ad9226a24b33ba17a2fb
ibrahim <ibrahimagrud7@gmail.com>
2026-08-10 14:20:35 +0300

    Save User Info wiht win. Registry

--- DVLD/Global Classes/Global.cs
+++ DVLD/Global Classes/Global.cs
@@ -12,9 +12,65 @@ namespace DVLD
 
         public static User CurrentUser = User.Find(1);
 
+        public static bool DeleteRegistrdLoginInfo()
+        {
+            string subKeyPath = @"Software\DVLD";
+            string valueName = "aa";
 
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(subKeyPath, writable: true))
+                {
+                    if (key == null)
+                    {
+                        return false;
+                    }
+
+                    // GetValueNames() içinde "aa" var mı diye kontrol edelim
+                    if (Array.IndexOf(key.GetValueNames(), valueName) >= 0)
+                    {
+                        key.DeleteValue(valueName);
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+     
+        
         public static bool RememberUsernameAndPassword(string userName, string password)
         {
+
+            if(userName==""||password=="")
+            {
+                return DeleteRegistrdLoginInfo();
+            }
+
+            string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
+            string valueName = userName;
+            string valueData = password;
+           
+            try
+            {
+                Registry.SetValue(keyPath, valueName, valueData, RegistryValueKind.String);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            
+            //save to file
+            /*
+             
             try
             {
                 string currentDirectory = System.IO.Directory.GetCurrentDirectory();
@@ -43,48 +99,100 @@ namespace DVLD
                 MessageBox.Show($"An error occurred: {ex.Message}");
                 return false;
             }
+             */
+
         }
+
+
 
         public static bool GetStoredCredential(ref string userName, ref string passwored)
         {
+
+            string subKeyPath = @"Software\DVLD";
+            string valueName = "";
+
             try
             {
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
 
-                string filePath = Path.Combine(currentDirectory, "loginData.txt");
-
-
-                if (File.Exists(filePath))
+                // 1. Adım: Key'i aç (sadece okuma amaçlı)
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(subKeyPath))
                 {
-
-                    using (StreamReader read = new StreamReader(filePath))
+                    if (key == null)
                     {
-                        string line;
-                        while ((line = read.ReadLine()) != null)
-                        {
-                            string[] splitedString = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
+                        return false;
 
-                            userName = splitedString[0];
-                            passwored = splitedString[1];
-
-                        }
-                        return true;
                     }
 
+                    // 2. Adım: Bu key altındaki TÜM value isimlerini al
+                    string[] valueNames = key.GetValueNames();
+
+                    if (valueNames.Length == 0)
+                    {
+                        return false;
+                    }
+
+                    // 3. Adım: Her isim için değerini al ve yazdır
+                    foreach (string name in valueNames)
+                    {
+                        object data = key.GetValue(name);
+                        userName = name;
+                        passwored = data.ToString();
+                       
+                    }
+                    return true;
                 }
-                else
-                {
-                    return false;
-                }
+            }
+            catch (Exception)
+            {
+                return false;
 
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}");
-                return false;
-            }
+          
+
         }
+
+
+        //get data from file
+        /*
+
+            try
+           {
+               string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+
+               string filePath = Path.Combine(currentDirectory, "loginData.txt");
+
+
+               if (File.Exists(filePath))
+               {
+
+                   using (StreamReader read = new StreamReader(filePath))
+                   {
+                       string line;
+                       while ((line = read.ReadLine()) != null)
+                       {
+                           string[] splitedString = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
+
+                           userName = splitedString[0];
+                           passwored = splitedString[1];
+
+                       }
+                       return true;
+                   }
+
+               }
+               else
+               {
+                   return false;
+               }
+
+           }
+           catch (Exception ex)
+           {
+               MessageBox.Show($"An error occurred: {ex.Message}");
+               return false;
+           }
+         */
+    }
 
     }
 
-}
